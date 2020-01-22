72a18842ac55592989d88c3ca41edf5daefdc636
Gustavo Henrique de Lima Perenciolo <gustavolima@lnxcit020062.cit>
2020-01-22 13:20:10 -0300

    Add multiple recipe call

--- contentalandoclient/pages/recipes/index.vue
+++ contentalandoclient/pages/recipes/index.vue
@@ -1,15 +1,21 @@
 <template>
   <b-container>
-    <b-row>
-      <b-col xl="12">
-        <h1>{{ title }}</h1>
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <div v-html="body" class="posts__body" />
-      </b-col>
-    </b-row>
+    <template v-if="recipes">
+      <b-row v-for="recipe in recipes.data" :key="recipe.id">
+        <b-col xl="12">
+          <h1>
+            <b-btn @click="open(recipe.links.self.href)">{{
+              recipe.attributes.title
+            }}</b-btn>
+          </h1>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <div class="posts__body" />
+        </b-col>
+      </b-row>
+    </template>
   </b-container>
 </template>
 
@@ -18,23 +24,31 @@ export default {
   components: {},
   async asyncData({ app }) {
     const data = await app.$axios
-      .get(
-        '/api/recipes/811ae8e5-0b28-418f-ab23-ea429d690495?resourceVersion=id%3A15',
-        {}
-      )
-      .then((res) => {
-        return {
-          title: res.data.data.attributes.title,
-          body: res.data.data.attributes.instructions
-        }
-      })
+      .get('/api/recipes', {})
+      .then(({ data: { data } }) => ({ recipes: { data } }))
       .catch((e) => {
         if (e) return e
       })
 
+    console.log(JSON.stringify(data))
+
     return data
+  },
+  methods: {
+    async open(path) {
+      if (!path) return
+
+      const data = await this.$axios
+        .$get(path, {})
+        .then(({ data }) => {
+          console.log(data)
+        })
+        .catch((e) => {
+          if (e) return e
+        })
+
+      console.log(data)
+    }
   }
 }
 </script>
-
-<style scoped></style>
